1029c3659f027e204b21efa8370210cc3d996eb4
dylan13579 <141684479+dylan13579@users.noreply.github.com>
2026-06-08 17:57:34 -0300

    elimino comentarios y algunos cambios

--- controllers/valoracionController.js
+++ controllers/valoracionController.js
@@ -10,35 +10,33 @@ async function votar(req, res) {
 
     const id_usuario = req.session.usuario.id;
 
-    console.log('BODY:', req.body); // DEBUG
+    console.log('BODY:', req.body);
 
-    // 🔎 obtener publicación
     const publicacion = await publicacionModel.obtenerPorId(id_publicacion);
 
     if (!publicacion) {
       return res.send('Publicación no encontrada');
     }
 
-    // 🚫 evitar que el autor vote
     if (publicacion.id_usuario === id_usuario) {
-      return res.send('No podés votar tu propia publicación');
+      return res.redirect(`/publicaciones/${id_publicacion}`);
     }
 
-    // 🔎 ver si ya votó
+
     const yaVoto = await valoracionModel.obtenerVoto(
       id_publicacion,
       id_usuario
     );
 
     if (yaVoto) {
-      // 🔁 actualizar voto
+
       await valoracionModel.actualizar(
         id_publicacion,
         id_usuario,
         puntuacion
       );
     } else {
-      // ➕ nuevo voto
+
       await valoracionModel.votar(
         id_publicacion,
         id_usuario,
